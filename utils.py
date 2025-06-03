51624dc9b85cb2d85a064ad41f0c58dfed868f22
june21a <june21r@gmail.com>
2025-06-03 19:22:56 +0900

    [refactor] : move make wrong group json function to utils.py

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import importlib
 import json
 import albumentations as A
 import torchvision.transforms as T
+from collections import defaultdict
 
 class Logger:
     def __init__(self, filepath):
@@ -95,3 +96,49 @@ def find_first_file_by_extension(directory, extension = '.pth'):
     # 사전순 정렬 후 첫 번째 파일 반환
     matched_files.sort()
     return os.path.join(directory, matched_files[0])
+
+
+def find_class_groups_from_jsons(folder_path, start_epoch = 10):
+    # 1. 클래스 간 연결 정보 추출
+    class_graph = defaultdict(set)
+
+    for file_name in os.listdir(folder_path):
+        if int(file_name.split('_')[3]) < start_epoch:
+            continue
+
+        if file_name.endswith(".json"):
+            file_path = os.path.join(folder_path, file_name)
+            with open(file_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+
+                for true_class, examples in data.items():
+                    for ex in examples:
+                        predicted_class = ex['model_answer']
+                        # 양방향 연결로 그래프 구성
+                        class_graph[true_class].add(predicted_class)
+                        class_graph[predicted_class].add(true_class)
+
+    # 2. 그래프 기반 그룹 찾기 (연결된 component 별로 묶기)
+    visited = set()
+    groups = []
+
+    def dfs(node, group):
+        visited.add(node)
+        group.append(node)
+        for neighbor in class_graph[node]:
+            if neighbor not in visited:
+                dfs(neighbor, group)
+
+    for cls in class_graph:
+        if cls not in visited:
+            group = []
+            dfs(cls, group)
+            groups.append(sorted(group))
+
+    return groups
+
+def get_total_wrong_groups(work_dir, start_epoch):
+    wrong_examples = os.path.join(work_dir, 'wrong_examples')
+    groups = find_class_groups_from_jsons(wrong_examples, start_epoch)
+    with open(os.path.join(work_dir, 'groups.json'), 'w') as f:
+        json.dump(groups, f)
